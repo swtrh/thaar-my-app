e7e6d684efaf40b090682d99796d61d8a7841205
unknown <thaar@LT-739097.statoil.net>
2015-09-21 14:13:19 +0200

    Added more support for store

--- routes/store.js
+++ routes/store.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('store', { title: 'Express Store' });
 });
 
+router.route('/message').post(function(req, res, next) {
+  console.log('Reveiving message ' + req.body.message);
+  res.send('Message was ' + req.body.message)
+});
+
 module.exports = router;
